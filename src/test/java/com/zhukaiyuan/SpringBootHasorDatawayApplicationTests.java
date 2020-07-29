7e985933f320bdc8ce445e44f7c22ffec1edaa03
zhukaiyuan <zhuky9527@qq.com>
2020-07-29 22:36:38 +0800

    init

---
+++ src/test/java/com/zhukaiyuan/SpringBootHasorDatawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhukaiyuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHasorDatawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
